5a059aefa7cc4a83d4820e541bd2dfa3154442f6
brendent62 <Djkrhodges@gmail.com>
2026-02-16 13:32:29 -0600

    Seperated the html files so i can set up paths. Added path parsing and path function to allow for paths.

--- server.c
+++ server.c
@@ -1,94 +1,123 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#define port 25565
-#include <arpa/inet.h>
-#include <string.h>
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <unistd.h>
+    #define port 22556
+    #include <arpa/inet.h>
+    #include <string.h>
 
+    void file_open(int c, char FilePath[10]){
+        FILE *f;
+        char fpath[256];
+        sprintf(fpath, "./pages/%s" ,FilePath);
+        f = fopen(fpath, "r");
+        if(!f){
+            perror("error opening file");
+            return;
+        }
+        fseek(f,0,SEEK_END);
+        long fsize = ftell(f);
+        fseek(f,0,SEEK_SET);
 
-
-int main(){
-    FILE *f;
-    f = fopen("Frontend.html", "r");
-    if(!f){
-        perror("error opening file");
-        return 1;
-    }
-    char Message[256] = "You have connceted";
-    int s;
-    s = socket(AF_INET,SOCK_STREAM,0);
-
-    if(s < 0){
-        perror("Socket creation failed");
-        return 1;
-    }
-    
-    struct sockaddr_in servAdd;
-    servAdd.sin_family = AF_INET;
-    servAdd.sin_port = htons(port);
-    servAdd.sin_addr.s_addr = inet_addr("127.0.0.1");
-    struct sockaddr_in ClientInfo;
-    socklen_t cli_len = sizeof(ClientInfo);
-    char page[2000000]= "HHTP";
-
-    int e1= bind(s, (struct sockaddr *)&servAdd, sizeof(servAdd));
-    if(e1<0){
-        perror("bind failed");
-        return 1;
-    }
-
-    listen(s,50);
-    printf("Server listening on http://127.0.0.1:%d\n", port);
-    int c;
-
-
-    fseek(f,0,SEEK_END);
-    long fsize = ftell(f);
-    fseek(f,0,SEEK_SET);
-    char *html = malloc(fsize + 1);
-    fread(html,1,fsize,f);
-    html[fsize] = 0;
-    char header[512];
-    if (!html) {
-        perror("Malloc failed");
-        close(c);
-        close(s);
+        char *html = malloc(fsize + 1);
+        fread(html,1,fsize,f);
+        html[fsize] = 0;
         fclose(f);
-        return 1;
+        char header[512];
+
+
+        sprintf(header,
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/html\r\n"
+                "Content-Length: %ld\r\n"
+                "Connection: close\r\n"
+                "\r\n",
+                fsize);
+
+        send(c, header, strlen(header), 0);
+        send(c, html, fsize, 0);
+        free(html);
+                
+ 
+
     }
 
 
-    sprintf(header,
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: text/html\r\n"
-            "Content-Length: %ld\r\n"
-            "Connection: close\r\n"
-            "\r\n",
-            fsize);
-    while(1){
-        c=accept(s,(struct sockaddr *)&ClientInfo, &cli_len);
-        if (c < 0) {
-            perror("Accept failed");
-            close(s);
-            fclose(f);
+    int main(){
+        
+        
+        
+        char Message[256] = "You have connceted";
+        int s;
+        s = socket(AF_INET,SOCK_STREAM,0);
+
+        if(s < 0){
+            perror("Socket creation failed");
+            return 1;
+
+        
+
+        }
+        
+        struct sockaddr_in servAdd;
+        servAdd.sin_family = AF_INET;
+        servAdd.sin_port = htons(port);
+        servAdd.sin_addr.s_addr = inet_addr("127.0.0.1");
+        struct sockaddr_in ClientInfo;
+        socklen_t cli_len = sizeof(ClientInfo);
+        char page[2000000]= "HHTP";
+
+        int e1= bind(s, (struct sockaddr *)&servAdd, sizeof(servAdd));
+        if(e1<0){
+            perror("bind failed");
             return 1;
         }
 
-        char request[4096];
-        int bytes_received = recv(c, request, sizeof(request) - 1, 0);
-        if (bytes_received > 0) {
-            request[bytes_received] = '\0';
-            printf("Request received:\n%s\n", request);
-        }
+        listen(s,50);
+        printf("Server listening on http://127.0.0.1:%d\n", port);
+        int c;
 
-        send(c, header, strlen(header), 0);
-        send(c,html, fsize, 0);
 
-        }
-    free(html);
-    close(s);
-    return 0;
-}
+        
+
+        while(1){
+            c = accept(s,(struct sockaddr *)&ClientInfo, &cli_len);
+            if (c < 0) {
+                perror("Accept failed");
+                close(s);
+                return 1;
+            }
+
+            char request[4096];
+            int bytes_received = recv(c, request, sizeof(request) - 1, 0);
+            if (bytes_received > 0) {
+                request[bytes_received] = '\0';
+                printf("Request received:\n%s\n", request);
+                char method[10];
+                char path[1024];
+                sscanf(request,"%s %s",method,path);
+
+                printf("Method %s\n", method);
+                printf("path %s\n", path);
+                if (strcmp(path, "/") == 0) {
+                    file_open(c, "home.html");
+                    }
+                else if (strcmp(path, "/projects")==0){
+                    file_open(c, "projects.html");
+                }
+                else if (strcmp(path, "/resume") ==0){
+                    file_open(c, "resume.html");
+                }
+
+            }
+ 
+    
+
+            }
+
+
+        close(s);
+        return 0;
+    }
